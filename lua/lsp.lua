7d1b8c3cd8c70ab1ba9313a59f7d1dc24cda717f
Jontze <42588836+jontze@users.noreply.github.com>
2022-07-23 14:07:25 +0200

    feat(lsp): Preinstall ts, json, lua and eslint language server

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -72,6 +72,14 @@ end
 
 local lsp_installer = require("nvim-lsp-installer")
 
+local servers = { 'cssls', 'eslint', 'jsonls', 'tailwindcss', 'tsserver', 'sumneko_lua'}
+for _, name in pairs(servers) do
+    local server_is_found, server = lsp_installer.get_server(name)
+    if server_is_found and not server:is_installed() then
+        server:install()
+    end
+end
+
 -- Register a handler that will be called for all installed servers.
 -- Alternatively, you may also register handlers on specific server instances instead (see example below).
 lsp_installer.on_server_ready(function(server)
