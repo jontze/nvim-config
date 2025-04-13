878f02dbe7c67349acbea241159699046ccc2358
Jontze <42588836+jontze@users.noreply.github.com>
2025-04-13 16:26:46 +0200

    feat(lsp):Register lsp server via old way

--- lua/mason-config.lua
+++ lua/mason-config.lua
@@ -12,16 +12,16 @@ require("mason-lspconfig").setup {
     ensure_installed = { "lua_ls", "bashls", "yamlls", "dockerls", "jsonls", "angularls", "html", "tailwindcss", "ts_ls", "terraformls", "ansiblels", "marksman" },
 }
 
-vim.lsp.config("lua_ls", {})
-vim.lsp.config("bash_ls", {})
-vim.lsp.config("yamlls", {})
-vim.lsp.config("dockerls", {})
-vim.lsp.config("jsonls", {})
-vim.lsp.config("angularls", {})
-vim.lsp.config("html", {})
-vim.lsp.config("tailwindcss", {})
-vim.lsp.config("ts_ls", {})
-vim.lsp.config("terraformls", {})
-vim.lsp.config("ansiblels", {})
-vim.lsp.config("marksman", {})
+require("lspconfig").lua_ls.setup{}
+require("lspconfig").bashls.setup{}
+require("lspconfig").yamlls.setup{}
+require("lspconfig").dockerls.setup{}
+require("lspconfig").jsonls.setup{}
+require("lspconfig").angularls.setup{}
+require("lspconfig").html.setup{}
+require("lspconfig").tailwindcss.setup{}
+require("lspconfig").ts_ls.setup{}
+require("lspconfig").terraformls.setup{}
+require("lspconfig").ansiblels.setup{}
+require("lspconfig").marksman.setup{}
 
